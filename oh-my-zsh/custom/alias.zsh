fb76d8c21c0d2724ea35c2dfd16f0966701c327b
diogopms <info@diogoserrano.com>
2017-10-15 19:07:39 +0100

    Add docker zsdh alias

--- oh-my-zsh/custom/alias.zsh
+++ oh-my-zsh/custom/alias.zsh
@@ -2,27 +2,3 @@
 alias sniff="sudo ngrep -W byline -d 'en0' -t '^(GET|POST) ' 'tcp and port 80'"
 
 alias fixyes="stty icrnl"
-
-#alias-docker
-
-ddebian() {
-  docker run -it debian
-}
-
-dstop() { docker stop $(docker ps -a -q); }
-
-drm() { docker rm $(docker ps -a -q); }
-
-dmrr() {
-  docker run -p 6379:6379 --name redis-server-p -d redis;
-  docker run -p 27017:27017 --name mongo-server-p -d mongo:2.6 --smallfiles;
-  docker run -p 5672:5672 -p 15672:15672 --name rabbit-server-p -d rabbitmq:3;
-}
-
-dvolumes() {
-  docker volume rm $(docker volume ls -qf dangling=true)
-}
-
-dnone() {
- docker rmi $(docker images | grep "^<none>" | awk "{print $3}")
-}
