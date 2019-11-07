4185e82932f6e8e15ad088b49f975b43f7ddca34
Diogo Serrano <info@diogoserrano.com>
2019-11-07 08:21:37 +0000

    Add exclude for kubernetes prod

--- kubernetes/aliases.zsh
+++ kubernetes/aliases.zsh
@@ -2,4 +2,4 @@
 # Install helm specific version
 # brew install https://raw.githubusercontent.com/Homebrew/homebrew-core/e5300686f0dffd97dfc2f1bb60cd8756894829ea/Formula/kubernetes-helm.rb
 alias logs="stern  --all-namespaces --exclude kube-probe -t --since 1h"
-alias logs-prod="stern -n gws-production --kubeconfig ~/.kube/config.prod -t --since 1h"
+alias logs-prod="stern -n gws-production --kubeconfig ~/.kube/config.prod --exclude kube-probe -t --since 1h"
