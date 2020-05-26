04a66dd761204af0889727c5e249248cda04071b
Diogo Serrano <info@diogoserrano.com>
2020-05-26 11:23:59 +0200

    Update aliases for kubernetes

--- kubernetes/aliases.zsh
+++ kubernetes/aliases.zsh
@@ -2,6 +2,9 @@
 # Install helm specific version
 # Download from https://get.helm.sh/helm-v2.16.1-darwin-amd64.tar.gz
 # Copy to /usr/log/bin
-
-alias logs="stern  --all-namespaces --exclude kube-probe -t --since 1h"
-alias logs-prod="stern -n gws-production --kubeconfig ~/.kube/config.prod --exclude kube-probe -t --since 1h"
+alias logs-grs-dev="stern --context DEV-STG-gws-services --exclude='kube-probe' --since 30m -n grs-development"
+alias logs-grs-stg="stern --context DEV-STG-gws-services --exclude='kube-probe' --since 30m -n grs-staging"
+alias logs-dev="stern --context DEV-STG-gws-services --exclude='kube-probe|instances to sync|listInstances' --since 30m -n gws-development"
+alias logs-stg="stern --context DEV-STG-gws-services --exclude='kube-probe|instances to sync|listInstances' --since 5m -n gws-staging"
+# alias logs-prd="stern --context PRD-gws-services --exclude='kube-probe|instances to sync|listInstances' --since 30m -n gws-production"
+alias logs-prd="stern --context PRD-gws-services --exclude='kube-probe' --since 5m -n gws-production"
