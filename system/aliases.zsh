8fd5d882164aac910348a8f2e8d1af2ecd39a491
Diogo Serrano <info@diogoserrano.com>
2021-11-17 14:54:50 +0000

    Update alias

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -9,3 +9,4 @@ alias google_dns='networksetup -setdnsservers "USB 10/100/1000 LAN" 8.8.8.8 8.8.
 alias opendns_dns='networksetup -setdnsservers "USB 10/100/1000 LAN" 208.67.222.222 208.67.220.220'
 alias cloudflare_dns='networksetup -setdnsservers "USB 10/100/1000 LAN" 1.1.1.1 1.0.0.1'
 alias pihole_dns='networksetup -setdnsservers "USB 10/100/1000 LAN" 10.13.0.82'
+alias my-watch="watch -n 1 '$1'"
