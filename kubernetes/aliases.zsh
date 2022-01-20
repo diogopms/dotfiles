4e4a290992737f1a29dc8b251a21ef683247ab75
Diogo Serrano <info@diogoserrano.com>
2022-01-20 14:45:03 +0000

    Remove comments

--- kubernetes/aliases.zsh
+++ kubernetes/aliases.zsh
@@ -1,7 +1,3 @@
-
-# Install helm specific version
-# Download from https://get.helm.sh/helm-v2.16.1-darwin-amd64.tar.gz
-# Copy to /usr/log/bin
 alias logs-grs-dev="stern --context DEV-STG-gws-services --exclude='kube-probe' --since 30m -n grs-development"
 alias logs-grs-stg="stern --context DEV-STG-gws-services --exclude='kube-probe' --since 30m -n grs-staging"
 alias logs-dev="stern --context DEV-STG-gws-services --exclude='kube-probe|instances to sync|listInstances' --since 30m -n gws-development"
