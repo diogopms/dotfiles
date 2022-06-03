3d8034e0fc4b2c967f4de565a069fb0b635356c7
Diogo Serrano <info@diogoserrano.com>
2022-06-03 16:26:48 +0100

    This commit is signed

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,3 +1,4 @@
 alias reload=". ~/.zshrc && echo 'ZSH config reloaded from ~/.zshrc'"
 alias cls='clear' # Good 'ol Clear Screen command
 alias vim='nvim'
+alias code="open -a Visual\ Studio\ Code.app"
